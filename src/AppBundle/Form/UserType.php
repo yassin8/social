78694467f0c69a2c1d1952611a9da21ec398e6c7
firas16 <firasfbs1989@yahoo.fr>
2016-12-21 19:22:51 +0100

    new user controller

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Firas
+ * Date: 21/12/2016
+ * Time: 19:04
+ */
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Course;
+use AppBundle\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('password')
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
+    }
+}
